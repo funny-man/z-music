865244bed1500f7d886a7717476a0411ca1e7da9
zeeliu <zeeliu666@163.com>
2018-04-15 22:59:51 +0800

    3

--- webpack.config.js
+++ webpack.config.js
@@ -1,39 +1,41 @@
 
-const webpack = require("webpack")
+const webpack = require("webpack"),
+      path = require("path")
+
 // CopyWebpackPlugin = require('copy-webpack-plugin'),
-// ExtractTextPlugin = require("extract-text-webpack-plugin"),
+ExtractTextPlugin = require("extract-text-webpack-plugin");
 // WebpackNotifierPlugin = require('webpack-notifier');
 
 
 module.exports = {
-    entry: __dirname + '/src/js/zMusic.js',
+    entry: path.resolve(__dirname, ' /src/js/zMusic.js '),
     output: {
-        path: __dirname + '/dist',//__dirname获取当前模块的绝对路径
+        path:path.resolve(__dirname, '/dist'),//__dirname获取当前模块的绝对路径
         filename: 'zMusic.min.js'
     },
-    resolve: {
-        alias: {
-            test: path.resolve(__dirname, 'test/test.js')
-        }
-    },
+    // resolve: {
+    //     alias: {
+    //         test: path.resolve(__dirname, 'test/test.js')
+    //     }
+    // },
     plugins: [
-        new webpack.ProvidePlugin({
-            $: 'jquery'
-        }),
-        new WebpackNotifierPlugin({
-            title: 'Webpack 编译成功',
-            contentImage: path.resolve(process.cwd(), './img/avatar.jpeg'),
-            alwaysNotify: true
-        }),
+        // new webpack.ProvidePlugin({
+        //     $: 'jquery'
+        // }),
+        // new WebpackNotifierPlugin({
+        //     title: 'Webpack 编译成功',
+        //     contentImage: path.resolve(process.cwd(), './img/avatar.jpeg'),
+        //     alwaysNotify: true
+        // }),
         new ExtractTextPlugin({
             filename: "[name].css",
             disable: false,
             allChunks: true
         }),
-        new webpack.optimize.CommonsChunkPlugin({
-            name: 'common',
-            minChunks: Infinity
-        })
+        // new webpack.optimize.CommonsChunkPlugin({
+        //     name: 'common',
+        //     minChunks: Infinity
+        // })
     ],
     module: {
         rules: [
@@ -54,18 +56,18 @@ module.exports = {
                     }
                 })
             },
-            {
-                test: /\.less$/,
-                use: ExtractTextPlugin.extract({
-                    fallback: 'style-loader',
-                    use: ['css-loader', {
-                        loader: 'less-loader',
-                        options: {
-                            sourceMap: true
-                        }
-                    }]
-                })
-            },
+            // {
+            //     test: /\.less$/,
+            //     use: ExtractTextPlugin.extract({
+            //         fallback: 'style-loader',
+            //         use: ['css-loader', {
+            //             loader: 'less-loader',
+            //             options: {
+            //                 sourceMap: true
+            //             }
+            //         }]
+            //     })
+            // },
             {
                 test: /\.scss$/,
                 use: ExtractTextPlugin.extract({
@@ -78,25 +80,25 @@ module.exports = {
                     }]
                 })
             },
-            {
-                test: /\.(png|jpg|jpeg|gif|woff|woff2|ttf|eot|svg|swf)$/,
-                use: {
-                    loader: 'file-loader',
-                    options: {
-                        name: '[name]_[sha512:hash:base64:7].[ext]'
-                    }
-                }
-            },
-            {
-                test: /\.html/,
-                use: {
-                    loader: "html-loader",
-                    options: {
-                        minimize: false,
-                        attrs: false
-                    }
-                }
-            }
+            // {
+            //     test: /\.(png|jpg|jpeg|gif|woff|woff2|ttf|eot|svg|swf)$/,
+            //     use: {
+            //         loader: 'file-loader',
+            //         options: {
+            //             name: '[name]_[sha512:hash:base64:7].[ext]'
+            //         }
+            //     }
+            // },
+            // {
+            //     test: /\.html/,
+            //     use: {
+            //         loader: "html-loader",
+            //         options: {
+            //             minimize: false,
+            //             attrs: false
+            //         }
+            //     }
+            // }
         ]
     }
 };
