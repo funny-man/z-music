a2a66eb1bbdf90110e1d4abd84fa66aafb1ccfae
zeeliu <zeeliu666@163.com>
2018-04-17 13:27:08 +0800

    webpack配置、js压缩插件 、sass转换抽离css插件，

--- src/js/zMusic.js
+++ src/js/zMusic.js
@@ -1,6 +1,10 @@
 //require('../sass/style.scss')
 import sass from '../sass/style.scss'
 
-console.log('nihaoa')
+console.log('可以了')
+console.log('终于配置完了')
 require('./public')
 require('./template')
+
+item => item+1
+
--- webpack.config.js
+++ webpack.config.js
@@ -1,111 +1,67 @@
 
-const webpack = require("webpack"),
-    path = require("path")
+const webpack = require("webpack");
+const path = require("path");
+const ExtractTextPlugin = require("extract-text-webpack-plugin");//外部插件要单独下载然后require；这个插件可以吧sass提取出来转成css而不是打包到js里面
 
-// CopyWebpackPlugin = require('copy-webpack-plugin'),
-ExtractTextPlugin = require("extract-text-webpack-plugin");
-// WebpackNotifierPlugin = require('webpack-notifier');
+var BUILD_PATH = path.resolve(__dirname, 'dist');
+var PATH = path.resolve(__dirname, 'src/js/zMusic.js');
+
+//写一个判断啥时候使用代码压缩
 
 
 module.exports = {
-    entry: path.resolve(__dirname, 'src/js/zMusic.js'),
+    entry: PATH,
     output: {
-        path: path.resolve(__dirname, 'dist'),//__dirname获取当前模块的绝对路径
+        path: BUILD_PATH,
         //包规范格式
         libraryTarget: 'umd',
         library: "zMusic",
         umdNamedDefine: true,
         filename: 'zMusic.min.js'
     },
-    devtool: 'source-map',
-    // resolve: {
-    //     alias: {
-    //         test: path.resolve(__dirname, 'test/test.js')
-    //     }
-    // },
+    // devtool: 'source-map', //每次处理生成.map文件便于在浏览器debug（压缩后的的代码不适与debug）
     plugins: [
-        // new webpack.ProvidePlugin({
-        //     $: 'jquery'
+        // new webpack.optimize.UglifyJsPlugin({  //这是一个吧js代码压缩的插件；该插件webpack提供不需要下载直接 webpack.optimize.UglifyJsPlugin；因为上面已经require了webpack
+        //     sourceMap: false
         // }),
-        // new WebpackNotifierPlugin({
-        //     title: 'Webpack 编译成功',
-        //     contentImage: path.resolve(process.cwd(), './img/avatar.jpeg'),
-        //     alwaysNotify: true
-        // }),
-        new webpack.HotModuleReplacementPlugin({
-            // Options...
-        }),
-        new ExtractTextPlugin({
+        new ExtractTextPlugin({   //该插件用于吧sass，lass，css 等文件淡出提取出来不是打包到js里面
             filename: "[name].css",
             disable: false,
             allChunks: true
         }),
-        // new webpack.optimize.CommonsChunkPlugin({
-        //     name: 'common',
-        //     minChunks: Infinity
-        // })
     ],
     module: {
         rules: [
             {
-                test: /\.js[x]?$/,
+                test: /\.js$/,
                 exclude: /node_modules/,
                 use: 'babel-loader'
             },
-            {
-                test: /\.css$/,
-                use: ExtractTextPlugin.extract({
-                    fallback: "style-loader",
-                    use: {
-                        loader: 'css-loader',
-                        options: {
-                            sourceMap: true
-                        }
-                    }
-                })
-            },
-            // {
-            //     test: /\.less$/,
-            //     use: ExtractTextPlugin.extract({
-            //         fallback: 'style-loader',
-            //         use: ['css-loader', {
-            //             loader: 'less-loader',
-            //             options: {
-            //                 sourceMap: true
-            //             }
-            //         }]
-            //     })
-            // },
+            //下面这段配置用了上面加载ExtractTextPlugin插件可以吧sass抽离出来
+            //意思是执行webpack后会单独产生一个css文件供index.html引用
             {
                 test: /\.scss$/,
+                exclude: /node_modules/,
                 use: ExtractTextPlugin.extract({
                     fallback: 'style-loader',
                     use: ['css-loader', {
                         loader: 'sass-loader',
                         options: {
-                            sourceMap: true
+                            sourceMap: false
                         }
                     }]
                 })
             },
+            //下面这段配置没有用插件只是单纯的吧sass问价按照'sass-loader','css-loader','style-loader'处理
+            //最后的文件打包到js文件一起index.html只要引用js文件就可以（入口js文件必须require或者importsass文件）
             // {
-            //     test: /\.(png|jpg|jpeg|gif|woff|woff2|ttf|eot|svg|swf)$/,
-            //     use: {
-            //         loader: 'file-loader',
-            //         options: {
-            //             name: '[name]_[sha512:hash:base64:7].[ext]'
-            //         }
-            //     }
-            // },
-            // {
-            //     test: /\.html/,
-            //     use: {
-            //         loader: "html-loader",
-            //         options: {
-            //             minimize: false,
-            //             attrs: false
-            //         }
-            //     }
+            //     test: /\.scss$/,
+            //     exclude: /node_modules/,
+            //     use: [
+            //         'style-loader',
+            //         'css-loader',
+            //         'sass-loader'
+            //     ]
             // }
         ]
     }
