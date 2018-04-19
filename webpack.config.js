d164356c601c625662da80d23c9f05b1c49d0d27
zeeliu <zeeliu666@163.com>
2018-04-19 17:00:43 +0800

    静态页面-20180419

--- webpack.config.js
+++ webpack.config.js
@@ -3,11 +3,9 @@ const webpack = require("webpack");
 const path = require("path");
 const ExtractTextPlugin = require("extract-text-webpack-plugin");//外部插件要单独下载然后require；这个插件可以吧sass提取出来转成css而不是打包到js里面
 
+
 var BUILD_PATH = path.resolve(__dirname, 'dist');
 var PATH = path.resolve(__dirname, 'src/js/zMusic.js');
-
-//写一个判断啥时候使用代码压缩
-
 
 module.exports = {
     entry: PATH,
@@ -29,6 +27,8 @@ module.exports = {
             disable: false,
             allChunks: true
         }),
+
+
     ],
     module: {
         rules: [
@@ -44,12 +44,21 @@ module.exports = {
                 exclude: /node_modules/,
                 use: ExtractTextPlugin.extract({
                     fallback: 'style-loader',
-                    use: ['css-loader', {
-                        loader: 'sass-loader',
-                        options: {
-                            sourceMap: false
-                        }
-                    }]
+                    use: [
+                        'css-loader', 
+                       
+                        {
+                            loader: 'postcss-loader',  //这是专门为样式加浏览器兼容前缀的loader
+                            options: {
+                                sourceMap: false,
+                                config: {path: 'postcss.config.js'}  // 这个得在项目根目录创建postcss.config.js文件在里面写好配置
+                            }
+                        },
+                        {
+                            loader: 'sass-loader', 
+                            options: {sourceMap: false}
+                        },
+                    ]
                 })
             },
             //下面这段配置没有用插件只是单纯的吧sass问价按照'sass-loader','css-loader','style-loader'处理
@@ -63,6 +72,16 @@ module.exports = {
             //         'sass-loader'
             //     ]
             // }
+            //下面是处理css得loader
+            // {
+            //     test: /\.css$/,
+            //     exclude: /node_modules/,
+            //     use:ExtractTextPlugin.extract({
+            //         fallback: 'style-loader',
+            //         use: ['css-loader', 'postcss-loader']
+            //     })
+            // },
+            //把图片文件抽离到dist文件夹；要不然sass里面要写绝对路径；写相对路径会失效
             {
                 test: /\.(png|jpg|jpeg|gif|woff|woff2|ttf|eot|svg|swf)$/,
                 use: {
@@ -72,6 +91,6 @@ module.exports = {
                     }
                 }
             },
-        ]
+        ]  
     }
 };
