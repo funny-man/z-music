1f8608744ebe6f61962e1f7249014df28806686d
zeeliu <zeeliu666@163.com>
2018-04-20 17:36:44 +0800

    0420修改0420修改0420修改0420修改0420修改0420修改0420修改0420修改0420修改0420修改0420修改0420修改0420修改0420修改0420修改0420修改0420修改0420修改0420修改0420修改0420修改0420修改0420修改0420修改0420修改0420修改0420修改0420修改0420修改0420修改0420修改0420修改0420修改0420修改0420修改0420修改0420修改0420修改0420修改0420修改0420修改0420修改0420修改0420修改0420修改0420修改0420修改0420修改0420修改0420修改0420修改0420修改0420修改0420修改0420修改0420修改0420修改0420修改0420修改0420修改0420修改0420修改0420修改0420修改0420修改0420修改0420修改0420修改0420修改0420修改0420修改0420修改0420修改0420修改0420修改0420修改0420修改0420修改0420修改0420修改0420修改0420修改0420修改0420修改0420修改0420修改0420修改0420修改0420修改0420修改0420修改0420修改0420修改0420修改0420修改0420修改0420修改0420修改0420修改0420修改0420修改0420修改0420修改0420修改0420修改0420修改0420修改0420修改0420修改0420修改0420修改0420修改0420修改0420修改0420修改0420修改0420修改0420修改0420修改0420修改0420修改0420修改0420修改0420修改0420修改0420修改0420修改0420修改0420修改0420修改0420修改0420修改0420修改0420修改0420修改0420修改0420修改0420修改0420修改0420修改0420修改0420修改0420修改0420修改0420修改0420修改0420修改0420修改0420修改0420修改0420修改0420修改0420修改0420修改0420修改0420修改0420修改0420修改0420修改0420修改0420修改0420修改0420修改0420修改0420修改0420修改0420修改0420修改0420修改0420修改0420修改0420修改0420修改0420修改0420修改0420修改0420修改0420修改0420修改0420修改0420修改0420修改0420修改0420修改0420修改0420修改0420修改0420修改0420修改0420修改0420修改0420修改0420修改0420修改0420修改0420修改0420修改0420修改0420修改0420修改0420修改0420修改0420修改0420修改0420修改0420修改0420修改0420修改0420修改0420修改0420修改0420修改0420修改0420修改0420修改0420修改0420修改0420修改0420修改0420修改0420修改0420修改0420修改0420修改0420修改0420修改0420修改0420修改0420修改0420修改0420修改0420修改0420修改0420修改0420修改0420修改0420修改0420修改0420修改0420修改0420修改0420修改0420修改0420修改0420修改0420修改0420修改0420修改0420修改0420修改0420修改0420修改0420修改0420修改0420修改0420修改0420修改0420修改0420修改0420修改0420修改0420修改0420修改0420修改0420修改0420修改0420修改0420修改0420修改0420修改0420修改0420修改0420修改0420修改0420修改0420修改0420修改0420修改0420修改0420修改0420修改0420修改0420修改0420修改0420修改0420修改0420修改0420修改0420修改0420修改0420修改0420修改0420修改0420修改0420修改0420修改0420修改0420修改0420修改0420修改0420修改0420修改0420修改0420修改0420修改0420修改0420修改0420修改0420修改0420修改0420修改0420修改0420修改0420修改0420修改0420修改0420修改0420修改0420修改0420修改0420修改0420修改0420修改0420修改0420修改0420修改0420修改0420修改0420修改0420修改0420修改0420修改0420修改0420修改0420修改0420修改0420修改0420修改0420修改0420修改0420修改0420修改0420修改0420修改0420修改0420修改0420修改0420修改0420修改0420修改0420修改0420修改0420修改0420修改0420修改0420修改0420修改0420修改0420修改

--- src/js/zMusic.js
+++ src/js/zMusic.js
@@ -1,10 +1,17 @@
-//require('../sass/style.scss')
-import sass from '../sass/style.scss'
+import '../sass/style.scss'
 
-console.log('可以了')
-console.log('终于配置完了')
 require('./public')
 require('./template')
 
-item => item+1
+// class{
+//     constructor(data){
+
+//     }
+//     init(){
+
+//     }
+//     bind(){
+        
+//     }
+// }
 
