56ed2c73f7fd0db59c324b9924dbf96fa36cd51c
zeeliu <zeeliu666@163.com>
2018-05-08 15:48:08 +0800

    压缩

--- webpack.config.js
+++ webpack.config.js
@@ -19,9 +19,9 @@ module.exports = {
     },
     // devtool: 'source-map', //每次处理生成.map文件便于在浏览器debug（压缩后的的代码不适与debug）
     plugins: [
-        // new webpack.optimize.UglifyJsPlugin({  //这是一个吧js代码压缩的插件；该插件webpack提供不需要下载直接 webpack.optimize.UglifyJsPlugin；因为上面已经require了webpack
-        //     sourceMap: false
-        // }),
+        new webpack.optimize.UglifyJsPlugin({  //这是一个吧js代码压缩的插件；该插件webpack提供不需要下载直接 webpack.optimize.UglifyJsPlugin；因为上面已经require了webpack
+            sourceMap: false
+        }),
         new ExtractTextPlugin({   //该插件用于吧sass，lass，css 等文件淡出提取出来不是打包到js里面
             filename: "[name].css",
             disable: false,
